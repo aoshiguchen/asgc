fe2ab37c7bdafd88be6e2ef8ff0b2155a73518c6
yangwen <1052045476@qq.com>
2018-05-07 20:32:08 +0800

    init

--- asgc-sys-provider/src/main/java/com/asgc/sys/provider/test/config/DubboConfig.java
+++ asgc-sys-provider/src/main/java/com/asgc/sys/provider/test/config/DubboConfig.java
@@ -3,8 +3,8 @@ package com.asgc.sys.provider.test.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.ImportResource;
 
-//@Configuration
-//@ImportResource(locations={"classpath:dubbo.xml"})
-//public class DubboConfig{
-//
-//}
+@Configuration
+@ImportResource(locations={"classpath:dubbo.xml"})
+public class DubboConfig{
+
+}
